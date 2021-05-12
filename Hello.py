d2bd33c75da5b8f70260d1bb77c04e3ef48a774d
Manisha,Bachu <pcarribean8@gmail.com>
2021-05-12 16:56:02 +0530

    Corrected the  python program

--- Hello.py
+++ Hello.py
@@ -1,6 +1,1 @@
-FROM ubuntu
-MAINTAINER MANISHA_BACHU
-RUN apt-get update
-RUN apt-get install python3 -y
-COPY Hello.py /var
-CMD python3 /var/Hello.py
+print("Hello ! This is my first python container")
